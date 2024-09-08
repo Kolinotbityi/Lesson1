096de894642783efe9db4b70426dcf7d86e6d5cc
Алеша <98235139+Kolinotbityi@users.noreply.github.com>
2024-09-08 23:43:17 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+example = "Приветствую, мое имя Алексей!"
+print(example[0]) #1
+
+print(example[-1])#2
+
+a = int(len(example) / 2)#3
+print(example[a:])
+
+print(example[::-1])#4
+
+print(example[::2])#5
